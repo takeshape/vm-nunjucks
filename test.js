36cad7e840e58fa656491a8e6ff3f6e6d4826639
Andrew Sprouse <sprouse.a@gmail.com>
2019-06-12 09:56:45 -0400

    Add jest tests

--- test.js
+++ test.js
@@ -3,9 +3,19 @@ const nunjucks = require('./lib/index');
 
 const env = new nunjucks.Environment(new nunjucks.FileSystemLoader(__dirname));
 
+it('renders from file', () => {
+    const html = env.render('test.html', {name: 'Nunjucks'});
+    expect(html).toBe('<h1>Hello World Nunjucks!</h1>\n');
+});
 
-const html = env.render('test.html', {name: 'Nunjucks'});
-const expected = '<h1>Hello World Nunjucks!</h1>\n';
-if (html !== expected) {
-    process.exit(1);
-}
+it('renders from string', () => {
+    const html = nunjucks.renderString('<h1>Hello World {{ name}}!</h1>', {name: 'Nunjucks'});
+    expect(html).toBe('<h1>Hello World Nunjucks!</h1>');
+});
+
+
+it('prevents access to env', () => {
+    const malicious = '{{ name.constructor.__proto__.constructor("return JSON.stringify(process.env)")() }}';
+    const html = nunjucks.renderString(malicious, {name: 'Nunjucks'});
+    expect(html === '{}').toBe(true);
+});
