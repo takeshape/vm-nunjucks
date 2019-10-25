a0562b554648af25918ea4869cc8653dea202e46
Andrew Sprouse <sprouse.a@gmail.com>
2019-10-24 23:45:02 -0400

    Use JS events instead of native to work around node v12 vm2 issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 'use strict';
 const webpack = require('webpack');
 const path = require('path');
+const NodeTemplatePlugin = require("webpack/lib/node/NodeTemplatePlugin");
+const LoaderTargetPlugin = require("webpack/lib/LoaderTargetPlugin");
+const FunctionModulePlugin = require("webpack/lib/FunctionModulePlugin");
+
+const builtins = require("module").builtinModules || Object.keys(process.binding("natives"));
+const polyfilled = new Set(['events']);
+const externals = builtins.filter(name => !polyfilled.has(name));
 
 module.exports = {
   entry: './src/nunjucks.js',
@@ -9,8 +16,15 @@ module.exports = {
     path: path.join(__dirname, 'lib'),
     filename: 'index.js'
   },
-  target: 'node',
-  node: false,
+  target: compiler => {
+    // Customized version of the "node" target from webpack/lib/WebpackOptionsApply.js
+    new NodeTemplatePlugin({
+      asyncChunkLoading: false
+    }).apply(compiler);
+    new FunctionModulePlugin().apply(compiler);
+    new LoaderTargetPlugin("node").apply(compiler);
+  },
+  externals,
   mode: 'production',
   plugins: [
     new webpack.IgnorePlugin(/^chokidar$/)
